f80dc31dd138a3686bae591fb9575b3ce2cec6ca
AbdiProg <abdul.haqani@gmail.com>
2021-10-09 12:45:34 +0200

    Iterative Version Fibonacci

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -3,7 +3,8 @@ public class FibonacciSequence {
 
 
     public static void main(String[]args){
-        sumOfEvenFibonacciIterative(100);
+        //sumOfEvenFibonacciIterative(100);
+        sumOfEvenFibonacciRecursive(100);
     }
 
     /**
@@ -25,11 +26,46 @@ public class FibonacciSequence {
             int newSecondNumber = firstNumber + secondNumber;
             firstNumber = secondNumber;
             secondNumber = newSecondNumber;
+            if(secondNumber % 2 == 0) sum = sum + secondNumber; //Falls secondNumber eine gerade Zahl darstellt, wird die Summe auf secondNumber draufaddiert.
             i++;
 
         }
 
 
     }
+
+
+    /**
+     *
+     * @param n
+     */
+    public static void sumOfEvenFibonacciRecursive( int n){
+
+        recursiveHelper(0, 1, n);
+
+    }
+
+    /**
+     *
+     * @param firstNumber
+     * @param secondNumber
+     * @param n
+     */
+    public static void recursiveHelper(int firstNumber, int secondNumber , int n){
+
+        if(n == 1){
+            return;
+        }
+
+        int tmpSum = firstNumber + secondNumber;
+        System.out.println(tmpSum);
+        firstNumber = secondNumber;
+        secondNumber = tmpSum;
+
+        recursiveHelper(firstNumber, secondNumber, n-1);
+
+    }
+
+
 
 }
